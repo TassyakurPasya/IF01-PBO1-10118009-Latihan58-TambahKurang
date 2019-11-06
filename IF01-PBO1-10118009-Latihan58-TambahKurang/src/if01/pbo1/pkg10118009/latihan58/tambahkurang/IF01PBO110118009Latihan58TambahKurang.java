6dcdc2eaaf874239be8c9d5d8b120a0b76e1ad4c
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-11-06 15:38:35 +0700

    Add files via upload

---
+++ IF01-PBO1-10118009-Latihan58-TambahKurang/src/if01/pbo1/pkg10118009/latihan58/tambahkurang/IF01PBO110118009Latihan58TambahKurang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pbo1.pkg10118009.latihan58.tambahkurang;
+
+/**
+ *
+ * @author user
+ */
+public class IF01PBO110118009Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlahbilangan = new JumlahBilangan();
+        jumlahbilangan.tampilHasilJumlah();
+        SelisihBilangan selisihBilangan = new SelisihBilangan();
+        selisihBilangan.tampilSelisih();
+    }
+    
+}
---
+++ IF01-PBO1-10118009-Latihan58-TambahKurang/src/if01/pbo1/pkg10118009/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pbo1.pkg10118009.latihan58.tambahkurang;
+
+/**
+ *
+ * @author user
+ */
+public class JumlahBilangan extends Bilangan{
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil Perjumlahan = "+(getX()+getY()));
+    }
+}
